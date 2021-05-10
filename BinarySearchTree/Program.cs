e768e3bd4decf74e703cca788347d151cef09b8b
vundelayamini <vundelayamini@gmail.com>
2021-05-10 16:34:12 +0530

    [yamini]Add: Welcome Message to binary search trees

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Message To Binary Search Trees");
+        }
+    }
+}
